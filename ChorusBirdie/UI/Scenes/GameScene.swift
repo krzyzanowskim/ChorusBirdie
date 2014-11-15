1efdf89fd41d031527efa6d206c08df05f70b6d0
Konrad Szczesniak <konrad.szczesniak@elpassion.pl>
2014-11-15 12:55:20 +0100

    added simple flying bird

--- ChorusBirdie/UI/Scenes/GameScene.swift
+++ ChorusBirdie/UI/Scenes/GameScene.swift
@@ -10,13 +10,24 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    let helloNode: SKSpriteNode = {
+        var helloNode = SKSpriteNode(imageNamed: "flying-bird")
+        helloNode.position = CGPointMake(400, 400)
+        helloNode.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(100, 100))
+        helloNode.physicsBody?.dynamic = true
+        helloNode.physicsBody?.mass = 0.2
+        return helloNode
+    }()
+    
     override func didMoveToView(view: SKView) {
-        
+        self.physicsWorld.gravity = CGVectorMake(0.0, -0.9)
+        self.addChild(helloNode)
+        helloNode.physicsBody?.applyForce(CGVectorMake(50.0, 0.0))
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        
+        helloNode.physicsBody?.applyImpulse(CGVectorMake(0.0, 20.0))
         
     }
    
