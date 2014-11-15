3e683a3e86e6249a2bf6928d9af6c045b2f74685
Konrad Szczesniak <konrad.szczesniak@elpassion.pl>
2014-11-15 14:55:42 +0100

    animating wings for flying bird

--- ChorusBirdie/UI/Nodes/FlyingBirdNode.swift
+++ ChorusBirdie/UI/Nodes/FlyingBirdNode.swift
@@ -11,6 +11,19 @@ import SpriteKit
 
 
 class FlyingBirdNode : SKSpriteNode {
+    
+    var topWing: SKSpriteNode = {
+        let node = SKSpriteNode(imageNamed: "flying-bird-wing-up")
+        node.anchorPoint = CGPointMake(0, 0)
+        return node
+    }()
+    
+    var bottomWing: SKSpriteNode = {
+        let node = SKSpriteNode(imageNamed: "flying-bird-wing-down")
+        node.anchorPoint = CGPointMake(1, 1)
+        return node
+    }()
+    
     class func bird() -> FlyingBirdNode {
         let node = FlyingBirdNode()
         
@@ -24,11 +37,29 @@ class FlyingBirdNode : SKSpriteNode {
     }
    
     override init() {
-        let birdTexture = SKTexture(imageNamed: "flying-bird")
+        let birdTexture = SKTexture(imageNamed: "flying-bird-body")
         super.init(texture: birdTexture, color: UIColor.whiteColor(), size: birdTexture.size())
+        self.addChild(topWing)
+        self.addChild(bottomWing)
     }
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
+    
+    func startAnimatingWings() {
+        let toTheRight = SKAction.rotateByAngle(0.5, duration: 0.2)
+        let toTheLeft = SKAction.rotateByAngle(-0.5, duration: 0.2)
+        let topSequence = SKAction.sequence([toTheRight, toTheLeft])
+        let bottomSequence = SKAction.sequence([toTheLeft, toTheRight])
+        
+        self.topWing.runAction(SKAction.repeatActionForever(topSequence))
+        self.bottomWing.runAction(SKAction.repeatActionForever(bottomSequence))
+    }
+    
+    func stopAnimatingWings() {
+        self.topWing.removeAllActions()
+        self.bottomWing.removeAllActions()
+    }
+    
 }
