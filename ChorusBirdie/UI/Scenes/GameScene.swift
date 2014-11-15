4836a8317b9a26f93cdb936a13bae85818dc31df
Konrad Szczesniak <konrad.szczesniak@elpassion.pl>
2014-11-15 16:33:43 +0100

    separate cables added to scene

--- ChorusBirdie/UI/Scenes/GameScene.swift
+++ ChorusBirdie/UI/Scenes/GameScene.swift
@@ -11,10 +11,16 @@ import SpriteKit
 class GameScene: SKScene {
     
     let birdNode = FlyingBirdNode.bird()
+    let cable1 = SKSpriteNode(imageNamed: "cable1")
+    let cable2 = SKSpriteNode(imageNamed: "cable2")
+    let cable3 = SKSpriteNode(imageNamed: "cable3")
+    let cable4 = SKSpriteNode(imageNamed: "cable4")
+
     
     override func didMoveToView(view: SKView) {
         self.physicsWorld.gravity = CGVectorMake(0.0, -0.9)
         self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        self.setupCables()
 
         self.addChild(birdNode)
         birdNode.physicsBody?.applyImpulse(CGVectorMake(5.0, 0.0))
@@ -34,5 +40,17 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    func setupCables() {
+        cable1.position = CGPointMake(self.size.width / 2 + 50, 300);
+        cable2.position = CGPointMake(self.size.width / 2, 200);
+        cable3.position = CGPointMake(self.size.width / 2, 120);
+        cable4.position = CGPointMake(self.size.width / 2, 50);
+        
+        self.addChild(cable1)
+        self.addChild(cable2)
+        self.addChild(cable3)
+        self.addChild(cable4)
     }
 }
